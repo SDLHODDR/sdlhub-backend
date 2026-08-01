00a84e6880a4855eb9702889665d79763498c791
Sunil Ghate <05510@sdloffice.com>
2026-08-01 09:30:07 +0530

    Gatepass

--- api/eportal/gatepass/gp_info_sv.php
+++ api/eportal/gatepass/gp_info_sv.php
@@ -81,7 +81,62 @@ try {
                 $returnArr
             );
         }
+    } else if($data['getGpAttddata']==true)
+    {
+        // $punchLogTbl = singRec("SELECT to_date (ATTD_DATE,'dd-mm-yyyy') AT_DATE,
+        // to_char (IN_TIME, 'HH24:MI:SS') IN_TIM,
+        // to_char (OUT_TIME, 'HH24:MI:SS') OUT_TIM,WORK_HOUR,OT_HOUR 
+        // FROM EPT_BCS_ATTD_REG
+        // WHERE emp_code='" . $data['emp_code'] . "' AND attd_date = '" . $data['gpass_date'] . "' ");
+
+        $punchLogTbl = singRec("SELECT to_date (ATTD_DATE,'dd-mm-yyyy') AT_DATE,
+        to_char (IN_TIME, 'HH24:MI:SS') IN_TIM,
+        to_char (OUT_TIME, 'HH24:MI:SS') OUT_TIM,WORK_HOUR,OT_HOUR 
+        FROM EPT_BCS_ATTD_REG
+        WHERE emp_code='" . $data['emp_code'] . "' AND attd_date = '04-Aug-22' ");
+
+        
+        $returnArr = [];
+        if($punchLogTbl || !empty($punchLogTbl))
+        {
+            //print_r($punchLogTbl);
+            if($data['out_type']=='FO')
+            {
+                $returnArr['keyRt'] = "Office in time";
+                $returnArr['valRt'] = $punchLogTbl['IN_TIM'];
+            } else if ($data['out_type']=='SO')
+            {
+                $returnArr['keyRt'] = "Office out time";
+                $returnArr['valRt'] = $punchLogTbl['OUT_TIM'];
+            } else if($data['out_type']=='OI')
+            {
+                $offDesk=singRec("select 
+                to_char(to_date(EPT_GET_OFFDESK_TIME('".$data['emp_code']."',
+                to_date('".$data['gpass_date']."')) - GET_OFFDESKTERRACE_TIME@EPPLIVE_LINK('".$data['emp_code']."',
+                to_date('".$data['gpass_date']."')), 'sssss') ,'hh24:mi') tm from dual ");
+
+                $terrace=singRec("select 
+                to_char(to_date(EPT_GET_TERRACE_TIME('".$data['emp_code']."',
+                to_date('".$data['gpass_date']."')) + GET_OFFDESKTERRACE_TIME@EPPLIVE_LINK('".$data['emp_code']."',
+                to_date('".$data['gpass_date']."')), 'sssss') ,'hh24:mi') tm from dual ");
+                                                                
+                $tmOffDesk = sum_the_time($offDesk['TM'].':00',$terrace['TM'].':00');
+
+                $returnArr['keyRt'] = "Out of office";
+                $returnArr['valRt'] = $tmOffDesk . " Hrs";
+            }
+
+            if($returnArr || !empty($returnArr)){
+                apiResponse(
+                    true,
+                    "Attance fetched successfully.",
+                    $returnArr
+                );
+            }
+        }
+
     }
+    exit;
 } catch (Throwable $e) {
     logOracleError($e);
     apiResponse(false, "Unable to fetch outdoorduty form data.", null, 500);
